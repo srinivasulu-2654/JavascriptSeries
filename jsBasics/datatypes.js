6007aa096f60ef04fcd3fc9f496ec613f547fb1e
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2024-03-13 12:13:38 +0000

    datatypes

--- jsBasics/datatypes.js
+++ jsBasics/datatypes.js
@@ -3,3 +3,23 @@
 // alert(3+3) ->(gives an error) // we are using nodejs, not browser
 
 console.log(3+3)
+
+let name = "Sreenu"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// Bigint
+// string => ""
+// Boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+
+// object
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); //object
