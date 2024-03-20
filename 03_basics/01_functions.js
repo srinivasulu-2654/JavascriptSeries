85f903ef82a7c3de2d494b3bdf2c44c87a2df531
srinivasulu-2654 <83912661+srinivasulu-2654@users.noreply.github.com>
2024-03-20 11:18:47 +0000

    functions02

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -48,4 +48,38 @@ function loginUserMessage(username) {
 }
 
 // console.log(loginUserMessage("sreenu"))
-console.log(loginUserMessage())  // output : undefined just logged in
+// console.log(loginUserMessage("Sreenu"))  // output : undefined just logged in
+
+function calculatCartPrice(val1,val2, ...num1){
+    return num1;
+}
+
+// console.log(calculatCartPrice(200,300,500, 2000));
+// console.log(typeof calculatCartPrice);
+
+const user = {
+    username: "Sreenu",
+    price : 199
+}
+
+function handleObject(anyobject)
+{
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user); // it is accessing even out of thee scope also becaues of (closure concept)
+
+ handleObject({
+    username: "Sam",
+    price:399
+ })
+
+ const mynewArray = [200,400,100,600]
+
+ function returnSecondValue(getArray) {
+    return getArray[1];
+ }
+
+//  console.log(returnSecondValue(mynewArray));
+
+console.log(returnSecondValue([200,400,500,1000]));
